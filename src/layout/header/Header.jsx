14ec1daa24034313a891ccfeafcfa6df6f561185
Enam <mdenamahmedchowdhury@gmail.com>
2023-12-03 10:40:55 +0600

    make header responsibe

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/images/logo-white.png";
 
 import {
@@ -12,6 +12,10 @@ import { HiPlus, HiDotsVertical } from "react-icons/hi";
 import HeaderItem from "../../component/headerItem/HeaderItem";
 
 const Header = () => {
+  // mini scrren toggle click state
+  const [hideMenuToggle, setHideMenuToggle] = useState(false);
+
+  // header menu item list
   const menu = [
     {
       name: "HOME",
@@ -44,10 +48,41 @@ const Header = () => {
       <div className="flex gap-8 items-center">
         <img src={logo} alt="" className="w-[80px] md:w=[115px] object-cover" />
 
-        {menu.map((item) => (
-          <HeaderItem name={item.name} Icon={item.icon} />
-        ))}
+        {/* large screen header */}
+        <div className="hidden md:flex gap-8">
+          {menu.map((item) => (
+            <HeaderItem name={item.name} Icon={item.icon} />
+          ))}
+        </div>
+
+        {/* mini screen header */}
+        <div className="flex md:hidden gap-5">
+          {menu.map(
+            (item, index) =>
+              index < 3 && <HeaderItem name={""} Icon={item.icon} />
+          )}
+          <div
+            className="md:hidden"
+            onClick={() => setHideMenuToggle(!hideMenuToggle)}
+          >
+            <HeaderItem name={""} Icon={HiDotsVertical} />
+
+            {/* mini screen toggle button */}
+            {hideMenuToggle ? (
+              <div className="absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 py-4">
+                {menu.map(
+                  (item, index) =>
+                    index > 2 && (
+                      <HeaderItem name={item.name} Icon={item.icon} />
+                    )
+                )}
+              </div>
+            ) : null}
+          </div>
+        </div>
       </div>
+
+      {/* right side profile image */}
       <img
         src="https://media.licdn.com/dms/image/C5603AQFO_bpgTYCePQ/profile-displayphoto-shrink_800_800/0/1612199719892?e=2147483647&v=beta&t=b9oJHXd2-U9BZ2mVZP5ZOa8iW6pexTc_bzQiaEYU5Yk"
         alt=""
